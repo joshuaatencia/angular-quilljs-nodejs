26f8b3eb0adeafbb064996b6dd2cd0f9515bb008
joshuaatencia <joshuaedts@gmail.com>
2021-03-12 21:28:39 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,94 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quill-test';
+  editor: any;
+  IMG_API_URL = 'http://localhost:3000/file';
+  file: any;
+  allImg = [];
+  prueba: string;
+  loading = false;
+
+  modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+      [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+
+      ['clean'],                                         // remove formatting button
+
+      ['image']                         // link and image, video
+    ]
+  };
+
+  constructor(private http: HttpClient) {
+    this.prueba = "assets/image"
+    this.loading = false;
+
+    this.http.get('http://localhost:3000/imagenes').subscribe(data => {
+
+      data['files'].forEach(element => {
+        this.allImg.push(`assets/image/${element}`);
+      });
+    });
+
+  }
+
+
+
+  EditorCreated(editor: any) {
+
+    editor.getModule('toolbar').addHandler('image', () => {
+
+      const input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/x-icon');
+      input.click();
+
+      input.onchange = () => {
+        let file = input.files[0];
+        console.log(file.name);
+        const range = editor.getSelection();
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          editor.insertEmbed(range.index, 'image', reader.result);
+        };
+
+        if (/^image\//.test(file.type)) {
+          this.file = file;
+          file = null;
+        } else {
+          console.warn('Solo puedes cargar imagenes');
+        }
+      };
+
+    });
+  }
+
+  onSubmit() {
+    if (this.file != null) {
+    this.loading = true;
+
+      let formData = new FormData();
+      formData.append('pathName', 'forage');
+      formData.append('file', this.file);
+      this.http.post('http://localhost:3000/file', formData)
+        .subscribe(event => {
+          console.log(event['path']);
+        });
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { QuillModule } from 'ngx-quill';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QuillModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
